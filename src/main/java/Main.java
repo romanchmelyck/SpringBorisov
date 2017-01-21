4164d87f9639a0d55cad18e007f14e315cfb1ef0
romanchmelyck <roman.chmelyck@gmail.com>
2017-01-21 23:50:07 +0200

    Switched  to xml context because of NPE in ContextEventListener

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,11 +5,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-		ApplicationContext ctx = new AnnotationConfigApplicationContext(PrjConfig.class);
-//		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("context.xml");
+//		ApplicationContext ctx = new AnnotationConfigApplicationContext(PrjConfig.class);
+		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("context.xml");
 		
 		Quoter terminator = ctx.getBean(Quoter.class);
-//		terminator.sayQuote();
+		terminator.sayQuote();
 		
 //		while(true) {
 //			Thread.sleep(100);
